1e908cfbddec707c8f798ddc840264f51bc93144
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2026-04-12 15:32:54 +0300

    Update 6.py

--- 6/6.py
+++ 6/6.py
@@ -9,6 +9,12 @@ def form_random_matrix(size: int) -> numpy.ndarray:
             matrix[i, j] = rng.random()
     return matrix
 
+def form_random_vector(size: int) -> numpy.ndarray:
+    vector = numpy.zeros(size)
+    for i in range(size):
+        vector[i] = rng.random()
+    return vector
+
 
 def multiply_matrix_vector(a_matrix: numpy.ndarray, b_vector: numpy.ndarray) -> numpy.ndarray:
     x_vector = numpy.zeros(b_vector.shape)
@@ -28,20 +34,24 @@ def lu_decomposition(matrix: numpy.ndarray) -> tuple[numpy.ndarray, numpy.ndarra
         u_matrix[i, i] = 1
 
     for k in range(size):
-        for i in range(size):
-            if i >= k:
-                l_matrix[i, k] = matrix[i, k] - numpy.sum([l_matrix[i, j] * u_matrix[j, k] for j in range(k)])
+        for i in range(k, size):
+            l_matrix[i, k] = matrix[i, k] - numpy.dot(l_matrix[i, :k], u_matrix[:k, k])
 
-        for i in range(size):
-            if i > k:
-                u_matrix[k, i] = 1 / l_matrix[k, k] * ( matrix[k, i] - numpy.sum([l_matrix[k, j] * u_matrix[j, i] for j in range(k)]) )
+        for j in range(k + 1, size):
+            u_matrix[k, j] = (matrix[k, j] - numpy.dot(l_matrix[k, :k], u_matrix[:k, j])) / l_matrix[k, k]
+
+        #for i in range(size):
+        #    if i >= k:
+        #        l_matrix[i, k] = matrix[i, k] - numpy.sum([l_matrix[i, j] * u_matrix[j, k] for j in range(k)])
+
+        #for i in range(size):
+        #    if i > k:
+        #        u_matrix[k, i] = 1 / l_matrix[k, k] * ( matrix[k, i] - numpy.sum([l_matrix[k, j] * u_matrix[j, i] for j in range(k)]) )
 
     return l_matrix, u_matrix
 
-def solve_ax_b(a_matrix: numpy.ndarray, b_vector: numpy.ndarray) -> numpy.ndarray:
-    size = a_matrix.shape[0]
-
-    l_matrix, u_matrix = lu_decomposition(a_matrix)
+def solve_lu_b(l_matrix: numpy.ndarray, u_matrix: numpy.ndarray, b_vector: numpy.ndarray) -> numpy.ndarray:
+    size = l_matrix.shape[0]
 
     z_vector = numpy.zeros(size)
     z_vector[0] = b_vector[0] / l_matrix[0, 0]
@@ -60,38 +70,42 @@ def solve_ax_b(a_matrix: numpy.ndarray, b_vector: numpy.ndarray) -> numpy.ndarra
 def iterative_refinement(
         a_matrix: numpy.ndarray,
         b_vector: numpy.ndarray,
-        x_vector_real: numpy.ndarray, x_vector_approximation: numpy.ndarray,
+        x_vector_approximation: numpy.ndarray,
         error: float
     ) -> numpy.ndarray:
 
+    l_matrix, u_matrix = lu_decomposition(a_matrix)
+
+    x_vector_approximation_refined = x_vector_approximation.copy()
+
     iteration_count = 0
     while True:
-        error_vector_real = x_vector_real - x_vector_approximation
-        discontinuity_vector = b_vector - multiply_matrix_vector(a_matrix, x_vector_approximation)
+        residual_vector = b_vector - multiply_matrix_vector(a_matrix, x_vector_approximation_refined)
 
-        error_vector_approximation = solve_ax_b(a_matrix, discontinuity_vector)
-        x_vector_approximation_refined = x_vector_approximation + error_vector_approximation
+        error_vector_approximation = solve_lu_b(l_matrix, u_matrix, residual_vector)
 
         if (
-            numpy.all(numpy.abs(error_vector_real) <= error) and
-            numpy.all( numpy.abs(multiply_matrix_vector(a_matrix, x_vector_approximation_refined) - b_vector ) <= error)
-        ) or iteration_count >= 100:
+            numpy.all(numpy.abs(error_vector_approximation) <= error) #and
+            #numpy.all( numpy.abs(multiply_matrix_vector(a_matrix, x_vector_approximation_refined) - b_vector ) <= error)
+        ) or iteration_count >= 1000:
             return x_vector_approximation_refined
 
-        x_vector_approximation = x_vector_approximation_refined
+        x_vector_approximation_refined += error_vector_approximation
 
         iteration_count += 1
 
 
 
 a_matrix = form_random_matrix(100) * 100
-x_vector = numpy.ones(100) * 2.5
+x_vector = form_random_vector(100) * 50 + 5
 b_vector = multiply_matrix_vector(a_matrix, x_vector)
 
-x_vector_approximation = solve_ax_b(a_matrix, b_vector)
-epsilon = numpy.max(multiply_matrix_vector(a_matrix, x_vector_approximation) - b_vector)
-print(epsilon)
+l_matrix, u_matrix = lu_decomposition(a_matrix)
 
-x_vector_refined_approximation = iterative_refinement(a_matrix, b_vector, x_vector_approximation, x_vector_approximation, 10**(-14))
-epsilon = numpy.max(multiply_matrix_vector(a_matrix, x_vector_refined_approximation) - b_vector)
-print(epsilon)
+x_vector_approximation = solve_lu_b(l_matrix, u_matrix, b_vector)
+epsilon = numpy.max(numpy.abs(multiply_matrix_vector(a_matrix, x_vector_approximation) - b_vector))
+print(f"Помилка без СЛАР: {epsilon}")
+
+x_vector_refined_approximation = iterative_refinement(a_matrix, b_vector, x_vector_approximation, 10**(-14))
+epsilon = numpy.max(numpy.abs(multiply_matrix_vector(a_matrix, x_vector_refined_approximation) - b_vector))
+print(f"Помилка із СЛАР: {epsilon}")
