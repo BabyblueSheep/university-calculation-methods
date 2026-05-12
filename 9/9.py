587492efb93ef2925d3f144e1bd6c1b791e55e59
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2026-05-12 18:05:55 +0300

    Update 9.py

--- 9/9.py
+++ 9/9.py
@@ -4,30 +4,26 @@ import matplotlib.pyplot as plot
 
 import numpy
 
+# []x1^2 + []x1 + []x2^2 + []x2 + [] = 0
 def f(x1, x2, coefficients):
     return (coefficients[0] * x1**2 + coefficients[1] * x1 +
             coefficients[2] * x2**2 + coefficients[3] * x2 +
-            coefficients[4] * (x2 - x1) + coefficients[5] * ((x2 - x1)**2) +
-            coefficients[6]
+            coefficients[4]
             )
 
-def f_solve_x2(x1, coefficients):
-    a, b, c, d, e, f, g = coefficients
+# []y^2 + []y + ([]x^2 + []x + []) = 0
+def f_y(x, coefficients):
+    a = coefficients[2]
+    b = coefficients[3]
+    c = x**2 * coefficients[0] + x * coefficients[1] + coefficients[4]
 
-    A = c + f
-    B = d + e - 2*f*x1
-    C = (a + f)*x1**2 + (b - e)*x1 + g
-
-    discriminant = B**2 - 4*A*C
-
+    discriminant = b**2 - 4 * a * c
     if discriminant < 0:
         return None, None
-    elif discriminant == 0:
-        return -B / (2*A), None
-    else:
-        x2_pos = (-B + numpy.sqrt(discriminant)) / (2*A)
-        x2_neg = (-B - numpy.sqrt(discriminant)) / (2*A)
-        return x2_pos, x2_neg
+
+    y_one = (-b + numpy.sqrt(discriminant)) / (2 * a)
+    y_two = (-b - numpy.sqrt(discriminant)) / (2 * a)
+    return y_one, y_two
 
 def rosenbrock_target(x1, x2):
     return 100 * (x2 - x1**2)**2 + (x1 - 1)**2
@@ -39,77 +35,71 @@ def hooke_jeeves_method(function_target, x_start):
     e_1 = 10**(-10)
     e_2 = 10**(-10)
 
-    x_step = numpy.array([0.5, 0.5])
-    x_first = x_start.copy()
-
     all_x_points = []
 
-    k = 0
-    for k in range(1000):
-        x_first = x_start.copy()
+    x_0 = x_start.copy()
+    x_1 = x_0.copy()
 
-        for i in range(len(x_start)):
+    x_step = numpy.array([0.1, 0.1])
+
+    k = 0
+    for k in range(10000):
+        x_1 = x_0.copy()
+        #x_step = numpy.array([0.1, 0.1])
+
+        for i in range(len(x_1)):
             while True:
-                x_first[i] = x_start[i] + x_step[i]
-                if function_target(x_first) < function_target(x_start):
+                x_1[i] = x_0[i] + x_step[i]
+                if function_target(x_1) < function_target(x_0):
                     break
 
-                x_first[i] = x_start[i] - x_step[i]
-                if function_target(x_first) < function_target(x_start):
+                x_1[i] = x_0[i] - x_step[i]
+                if function_target(x_1) < function_target(x_0):
                     break
 
                 x_step[i] /= q
                 if x_step[i] < e_1:
-                    x_first[i] = x_start[i]
+                    x_1[i] = x_0[i]
                     break
 
-        all_x_points.append(x_start)
-        if numpy.max(numpy.abs(x_first - x_start)) < e_2:
+        all_x_points.append(x_1)
+        if numpy.max(numpy.abs(x_1 - x_0)) < e_2:
             break
 
-        x_second = x_first.copy()
-        x_first = x_start.copy()
-
         while True:
-            x_second_start = x_second + p * (x_second - x_first)
-            x_second_probe = x_second_start.copy()
+            x_2_initial = x_1 + p * (x_1 - x_0)
+            x_2 = x_2_initial.copy()
 
-            for i in range(len(x_second_probe)):
+            for i in range(len(x_2)):
                 while True:
-                    x_second_probe[i] = x_second_start[i] + x_step[i]
-                    if function_target(x_second_probe) < function_target(x_second_start):
+                    x_2[i] = x_2_initial[i] + x_step[i]
+                    if function_target(x_2) < function_target(x_2_initial):
                         break
 
-                    x_second_probe[i] = x_second_start[i] - x_step[i]
-                    if function_target(x_second_probe) < function_target(x_second_start):
+                    x_2[i] = x_2_initial[i] - x_step[i]
+                    if function_target(x_2) < function_target(x_2_initial):
                         break
 
-                    x_second_probe[i] = x_second_start[i]
+                    x_2[i] = x_2_initial[i]
                     break
 
-            if numpy.max(numpy.abs(x_second_probe - x_second_start)) < e_2:
+            x_0 = x_1.copy()
+            if function_target(x_2) < function_target(x_1):
+                x_1 = x_2.copy()
+                continue
+            else:
                 break
-            if function_target(x_second_probe) >= function_target(x_second):
-                break
 
-            x_first = x_second.copy()
-            x_second = x_second_probe.copy()
-
-        if function_target(x_second) < function_target(x_first):
-            x_start = x_second.copy()
-        else:
-            x_start = x_first.copy()
-
-    all_x_points.append(x_start)
-    return x_start, k, all_x_points
+    all_x_points.append(x_1)
+    return x_1, k, all_x_points
 
 
 
 
-# []x1^2 + []x1 + []x2^2 + []x2 + [](x2 - x1) + [](x2 - x1)^2 + [] = 0
+# []x1^2 + []x1 + []x2^2 + []x2 + [] = 0
 coefficients = [
-    [-4, -4, 0, -5, 0, 5, 3],
-    [0, 9, -5, 4, -5, 0, 1]
+    [-0.3, -5.0, 0.4, -2.5, -1.0],
+    [-0.5, -2.1, 0.8, 3.3, 0.5]
 ]
 
 x1_values = numpy.linspace(-2, 2, 50)
@@ -133,8 +123,8 @@ f_values = numpy.array([[f(x1, x2, coefficients[0])**2 + f(x1, x2, coefficients[
 plot.contourf(x1_values, x2_values, f_values, locator=matplotlib.ticker.LogLocator())
 plot.colorbar()
 
-found_x, found_k, found_x_all = hooke_jeeves_method(lambda x: f(x[0], x[1], coefficients[0])**2 + f(x[0], x[1], coefficients[1])**2, numpy.array([0.0, 0.0]))
-print(f"Given starting point {[0.0, 0.0]} found point at {found_x} in {found_k} iterations (system of equations)")
+found_x, found_k, found_x_all = hooke_jeeves_method(lambda x: f(x[0], x[1], coefficients[0])**2 + f(x[0], x[1], coefficients[1])**2, numpy.array([-2.0, 1.0]))
+print(f"Given starting point {[-2.0, 1.0]} found point at {found_x_all[-1]} in {found_k} iterations (system of equations)")
 
 plot.plot([x[0] for x in found_x_all], [x[1] for x in found_x_all], marker="o", color="red")
 
@@ -142,10 +132,43 @@ plot.show()
 
 
 
+x_values = numpy.linspace(-2, 2, 1000)
+x_values_one = []
+x_values_two = []
+y_values_one = []
+y_values_two = []
+for i in range(len(x_values)):
+    x = x_values[i]
+    y_one, y_two = f_y(x, coefficients[0])
+    if y_one is not None:
+        x_values_one.append(x)
+        y_values_one.append(y_one)
+    if y_two is not None:
+        x_values_two.append(x)
+        y_values_two.append(y_two)
 
+plot.plot(x_values_one, y_values_one, color="blue")
+plot.plot(x_values_two, y_values_two, color="blue")
 
-#plot.plot([x[0] for x in found_x_all], [x[1] for x in found_x_all], marker="o", color="red")
-plot.plot(found_x[0], found_x[1], marker="o", color="red")
+x_values_one = []
+x_values_two = []
+y_values_one = []
+y_values_two = []
+for i in range(len(x_values)):
+    x = x_values[i]
+    y_one, y_two = f_y(x, coefficients[1])
+    if y_one is not None:
+        x_values_one.append(x)
+        y_values_one.append(y_one)
+    if y_two is not None:
+        x_values_two.append(x)
+        y_values_two.append(y_two)
+
+plot.plot(x_values_one, y_values_one, color="green")
+plot.plot(x_values_two, y_values_two, color="green")
+
+plot.plot([x[0] for x in found_x_all], [x[1] for x in found_x_all], marker="o", markersize=4, linewidth=0.5, color="maroon")
+plot.plot(found_x_all[-1][0], found_x_all[-1][1], marker="o", color="red")
 
 plot.axhline(0, color="black")
 plot.axvline(0, color="black")
